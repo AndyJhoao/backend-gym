cb308b53f9aa281fd044dddb65c796a9779b1957
AndyJhoao <andy_joao@live.com>
2021-03-26 15:55:45 -0600

    Configuracion Inicial, punto de partida para desarrollar GYM app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
-
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background-image: url("C:/Users/Andy_/Desktop/8voSemestre/SENSORES/PRACTICA1/ProyectoGym/ProyectoGYM/project-gym/src/assets/background-gym.jpg");
+  background-size: cover;
+}
+
+#nav {
+  padding: 30px;
+  background-color: #666666;
+}
+
+#nav a {
+  font-weight: bold;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/server.js
@@ -0,0 +1,35 @@
+"use strict";
+const express = require("express");
+// const path = require("path");
+const app = express();
+const cors = require("cors");
+// const mongoose = require("mongoose");
+
+const port = process.env.port || 5000;
+// mongoose
+//   .connect("mongodb://localhost/gym-databse", {
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//   })
+//   .then((db) => console.log("DB Connect"))
+//   .catch((err) => console.error(err));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Holla");
+});
+
+app.post("/signin", (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(port, (err) => {
+  if (err) return console.log(err);
+  console.log(`server running on port ${port}`);
+});
