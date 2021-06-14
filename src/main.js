ed0ca705cda7fff049e31eb7d46db602e2acfe8d
Andy Jhoao <andy_joao@live.com>
2021-06-14 11:21:08 -0500

    quitando firebase

--- src/main.js
+++ src/main.js
@@ -7,18 +7,6 @@ Vue.use(VueSwal);
 
 Vue.config.productionTip = false;
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBd6O6Ls9ARemH1ABEWRlXczHSCDnGiRn4",
-  authDomain: "gym-project-7014c.firebaseapp.com",
-  projectId: "gym-project-7014c",
-  storageBucket: "gym-project-7014c.appspot.com",
-  messagingSenderId: "66316355461",
-  appId: "1:66316355461:web:592849dd8357cf952fa6ec",
-  measurementId: "G-J5R4DK8SKH",
-};
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 new Vue({
   router,
   render: (h) => h(App),
